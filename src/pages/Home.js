3870baf863ca0d587b1a69722782036fe92dfe36
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2024-10-31 17:53:02 +0600

    last

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import { Container } from "reactstrap";
 import Base from "../components/Base";
 import NewFeed from "../components/NewFeed";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const Home = () => {
   return (
     <Base>
       <Container className="mt-3">
-        <NewFeed />
+        {/* <NewFeed /> */}
+        <Login />
+        {/* <Signup /> */}
       </Container>
     </Base>
   )
